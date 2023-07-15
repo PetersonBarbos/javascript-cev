7d51959d477297dea884d93432c6b2d3957708cf
PetersonBarbos <128330730+PetersonBarbos@users.noreply.github.com>
2023-07-15 18:55:57 -0300

    .

---
+++ Aula 11 condições/exer01.js
@@ -0,0 +1,8 @@
+//exemplo para praticar
+
+var vel = 60
+console.log(`A velocidade do carro é ${vel}km/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida, multado!!`)
+}
+console.log(`Dirija sempre na velocidade permitida`)
