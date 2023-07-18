7c47d55abd9e2d1b06c33ab1012eddfb305878d2
PetersonBarbos <128330730+PetersonBarbos@users.noreply.github.com>
2023-07-18 05:02:57 -0300

    .

--- exercicios repetiçao/contagem.js
+++ exercicios repetiçao/contagem.js
@@ -3,7 +3,14 @@ function contagem(){
     var fim = window.document.querySelector('input#fim')
     var passo = window.document.querySelector('input#passo')
     var res = window.document.getElementById('res')
-    if(Number(passo.value) == 0|| Number(inicio.value) > Number(fim.value)){
-        window.alert('Por favor, digite números corretos')
+    var iniciof = Number(inicio.value)
+    var fimf = Number(fim.value)
+    var passof = Number(passo.value)
+    if(Number(passo.value) == 0){
+        window.alert('passo inválido, vou considerar passo 1')
+        passo = 1
+    }
+    for(iniciof;iniciof<=fimf;iniciof+=passof){
+        res.innerHTML = (`${iniciof} <br>`)//ele tá sempre substituindo o res anterior pelo atual
     }
 }
