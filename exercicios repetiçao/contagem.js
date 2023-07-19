f8e05024fc0d8c7dd76a80c67c83a9db246529aa
PetersonBarbos <128330730+PetersonBarbos@users.noreply.github.com>
2023-07-19 04:26:28 -0300

    .

--- exercicios repetiçao/contagem.js
+++ exercicios repetiçao/contagem.js
@@ -6,11 +6,21 @@ function contagem(){
     var iniciof = Number(inicio.value)
     var fimf = Number(fim.value)
     var passof = Number(passo.value)
+    //para adcionar emoji é \u{1f449} e só sunciona entre crase
     if(Number(passo.value) == 0){
         window.alert('passo inválido, vou considerar passo 1')
         passo = 1
-    }
-    for(iniciof;iniciof<=fimf;iniciof+=passof){
-        res.innerHTML = (`${iniciof} <br>`)//ele tá sempre substituindo o res anterior pelo atual
+    } else{
+        if(fimf > iniciof){
+            for(iniciof;iniciof<=fimf;iniciof+=passof){
+                res.innerHTML += (` ${iniciof} \u{1f449}`)//ele tá sempre substituindo o res anterior pelo atual, pq eu coloquei = ao invés de +=
+            } 
+           
+        }else {
+                for(iniciof;iniciof>=fimf;iniciof-=passof){
+                res.innerHTML += (` ${iniciof} \u{1f449}`)
+                }
+        }
+        res.innerHTML += `\u{1f3c1}`
     }
 }
